826e84b0aa5cd8b4d26f9dbe8b72fc9109c7cf76
“Abhishek <abhishekpipriye2507@gmail.com>
2025-01-02 10:30:27 +0530

    added vercel file

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,17 @@
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import About from "./components/About";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Resume from "./components/Resume";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
-
 function App() {
-  
   return (
     <Router>
-      <Navbar />
       <Routes>
-        {/* Define routes for different pages */}
-        <Route path="/" element={<About />} />
-        <Route path="/resume" element={<Resume />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-      <Footer />
     </Router>
   );
 }
